b9bea820123906b75fd682a978789004117e7146
Daniel <daniel@d2hydro.nl>
2022-01-21 13:29:07 +0100

    Create read_metadata.py

---
+++ scripts/read_metadata.py
@@ -0,0 +1,15 @@
+"""Example script to read and validate metadata.json."""
+
+import json
+from pathlib import Path
+from jsonschema import validate
+
+metadata_json = Path(r"..\data\de_tol_small\metadata.json")
+schema_json = Path(r"..\json-schemas\metadata-schema.json")
+
+
+metadata = json.loads(metadata_json.read_text())
+schema = json.loads(schema_json.read_text())
+
+validate(instance=metadata, schema=schema)
+ 
